e97f1a2578f68790d9e221d1e4700a1c4f04a9f2
Anıl KAN <merhaba@anilkan.xyz>
2018-04-12 02:24:26 +0300

    form with controlled input cleared

--- src/views/TForm/TForm.js
+++ src/views/TForm/TForm.js
@@ -1,14 +1,76 @@
 import React, { Component } from 'react';
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Col,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from 'reactstrap';
 
 class TForm extends Component {
 	constructor(props) {
-    super(props);
+    	super(props);
+
+    	this.state = {
+    		formUsername: '',
+    		formName: ''
+    	}
+
+    	this.clearSearchForm = this.clearSearchForm.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+	}
+
+	  handleChange({ target }) {
+	    this.setState({
+	      [target.name]: target.value
+	    });
+	  }
+
+	clearSearchForm(e){
+		this.setState({
+			formUsername: '',
+			formName: ''
+		});
 	}
 
 	render() {
-  	return (
-      <div></div>
-  	);
+	  	return (
+	<div className="animated fadeIn">
+        <Row>
+          <Col xs="12" sm="6">
+            <Card>
+              <CardBody>
+                <Row>
+                  <Col xs="12">
+                    <FormGroup>
+                      <Label htmlFor="name">Username</Label>
+                      <Input type="text" id="username" name="formUsername" value={this.state.formUsername}  onChange={this.handleChange}/>
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs="12">
+                    <FormGroup>
+                      <Label htmlFor="ccnumber">Name</Label>
+                      <Input type="text" id="name" name="formName" value={this.state.formName}  onChange={this.handleChange}/>
+                    </FormGroup>
+                  </Col>
+                </Row>
+              </CardBody>
+              <CardFooter>
+              	<Button>Ara</Button>{' '}
+                <Button onClick={this.clearSearchForm}>Temizle</Button>
+              </CardFooter>
+            </Card>
+          </Col>
+        </Row>
+    </div>
+	  	);
 	}
 }
 
