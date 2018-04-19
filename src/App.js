bd477a6335bfe7b9be7e246087111ac4a87ba2d8
Anıl KAN <merhaba@anilkan.xyz>
2018-04-19 22:29:55 +0300

    Clean project for development

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
+import React, { Component } from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
 // Styles
 // Import Flag Icons Set
-import 'flag-icon-css/css/flag-icon.min.css';
+import "flag-icon-css/css/flag-icon.min.css";
 // Import Font Awesome Icons Set
-import 'font-awesome/css/font-awesome.min.css';
+import "font-awesome/css/font-awesome.min.css";
 // Import Simple Line Icons Set
-import 'simple-line-icons/css/simple-line-icons.css';
+import "simple-line-icons/css/simple-line-icons.css";
 // Import Main styles for this application
-import './scss/style.css'
+import "./scss/style.css";
 // import '../node_modules/@coreui/styles/scss/_dropdown-menu-right.scss';
 
 // Containers
-import { Full } from './containers';
-// Pages
-import { Login, Page404, Page500, Register } from './views/Pages';
+import { Full } from "./containers";
 
 // import { renderRoutes } from 'react-router-config';
 
@@ -24,10 +22,6 @@ class App extends Component {
     return (
       <HashRouter>
         <Switch>
-          <Route exact path="/login" name="Login Page" component={Login} />
-          <Route exact path="/register" name="Register Page" component={Register} />
-          <Route exact path="/404" name="Page 404" component={Page404} />
-          <Route exact path="/500" name="Page 500" component={Page500} />
           <Route path="/" name="Home" component={Full} />
         </Switch>
       </HashRouter>
